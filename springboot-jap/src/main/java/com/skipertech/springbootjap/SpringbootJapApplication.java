9d5e3b8c3a54cb41fd31f14794addbcb1f7d0830
anu-r.ag <anuragsawai6@gmail.com>
2024-02-20 15:10:53 +0530

    Swagger Documentation added

--- springboot-jap/src/main/java/com/skipertech/springbootjap/SpringbootJapApplication.java
+++ springboot-jap/src/main/java/com/skipertech/springbootjap/SpringbootJapApplication.java
@@ -1,9 +1,12 @@
 package com.skipertech.springbootjap;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(info = @Info(version = "v1",title = "Spring boot JPA +Exception Handling Repository"))
 public class SpringbootJapApplication {
 
 	public static void main(String[] args) {
@@ -11,3 +14,4 @@ public class SpringbootJapApplication {
 	}
 
 }
+
--- springboot-jap/src/main/java/com/skipertech/springbootjap/controller/CloudController.java
+++ springboot-jap/src/main/java/com/skipertech/springbootjap/controller/CloudController.java
@@ -2,6 +2,9 @@ package com.skipertech.springbootjap.controller;
 
 import com.skipertech.springbootjap.model.CloudVender;
 import com.skipertech.springbootjap.service.CloudService;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
@@ -9,6 +12,8 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/cloudvendor")
+@Tag(name = "JPA + Exception Handling ", description = "CRUD opration using Spring JPA and Exception handling ")
+@ApiResponses(value = {@ApiResponse(responseCode = "200", description = "Payload received "), @ApiResponse(responseCode = "400", description = "Bad Request")})
 @RequiredArgsConstructor
 public class CloudController {
 
@@ -16,11 +21,13 @@ public class CloudController {
     private final CloudService cloudService;
 
     @GetMapping("/vendor/{vendorId}")
-    public CloudVender getCloudvender(@PathVariable String vendorId)  {
+    public CloudVender getCloudvender(@PathVariable String vendorId) {
 
 
         return cloudService.fetchCloudVendor(vendorId);
-    }@GetMapping("/vendor")
+    }
+
+    @GetMapping("/vendor")
     public List<CloudVender> fetchCloudvender() {
 
 
@@ -43,7 +50,7 @@ public class CloudController {
     }
 
     @DeleteMapping("/{vendorId}")
-    public String deleteVender(@PathVariable  String vendorId) {
+    public String deleteVender(@PathVariable String vendorId) {
         return cloudService.deleteVendor(vendorId);
     }
 
